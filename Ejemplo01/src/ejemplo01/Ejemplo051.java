7228f7cdfb55c60c33efe57d57fb84d6ecc431df
teoperalta <teo.peralta.2007@gmail.com>
2025-10-29 12:15:10 -0500

    ejemplo 0501

---
+++ Ejemplo01/src/ejemplo01/Ejemplo051.java
@@ -0,0 +1,33 @@
+/*
+René Rolando
+
+Elizalde Solano
+
+1983
+ */
+package ejemplo01;
+
+/**
+ *
+ * @author reroes
+ */
+public class Ejemplo051 {
+    public static void main(String[] args) {
+        // Secuencia de escape \n y \t
+        String nombreEstudiante = "René Rolando";
+        String apellidoEstudiante = "Elizalde Solano";
+        int nacimiento = 1983;
+        
+        System.out.println(nombreEstudiante+"\n"+apellidoEstudiante+ "\n"+ 
+                nacimiento);
+        
+        System.out.println(nombreEstudiante+"\n\n"+apellidoEstudiante+ "\n\n"+ 
+                nacimiento);
+        
+        System.out.println(nombreEstudiante+"\n"+"\n"+apellidoEstudiante+ "\n"+
+                "\n"+ nacimiento); 
+        // System.out.println(nombreEstudiante+"\t"+apellidoEstudiante);   
+        // System.out.println(nombreEstudiante+"\n\t"+apellidoEstudiante);
+        // \n=siguiente linea \t=tab separa 4 espacios 
+    }
+}
