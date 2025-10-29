35702d97748a9ce916f9500d868828c8b6eccf58
teoperalta <teo.peralta.2007@gmail.com>
2025-10-29 12:11:37 -0500

    \t,\n

--- Ejemplo01/src/ejemplo01/Ejemplo05.java
+++ Ejemplo01/src/ejemplo01/Ejemplo05.java
@@ -27,6 +27,7 @@ public class Ejemplo05 {
         System.out.println(nombreEstudiante+"\n"+"\n"+apellidoEstudiante+ "\n"+
                 "\n"+ nacimiento); */
         System.out.println(nombreEstudiante+"\t"+apellidoEstudiante);   
-        System.out.println(nombreEstudiante+"\n\t"+apellidoEstudiante);   
+        System.out.println(nombreEstudiante+"\n\t"+apellidoEstudiante);
+        // \n=siguiente linea \t=tab separa 4 espacios 
     }
 }
