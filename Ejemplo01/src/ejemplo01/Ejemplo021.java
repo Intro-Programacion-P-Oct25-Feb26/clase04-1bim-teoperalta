a44447b7fcb0369ecb49f4e88e18471bd8746da9
teoperalta <teo.peralta.2007@gmail.com>
2025-10-29 11:53:42 -0500

    Snake case

---
+++ Ejemplo01/src/ejemplo01/Ejemplo021.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejemplo01;
+
+/**
+ *
+ * @author reroes
+ */
+public class Ejemplo021 {
+    public static void main(String[] args) {
+        // Ejemplo 
+        String nombre_estudiante = "René Rolando";
+        String apellido_estudiante = "Elizalde Solano";
+                
+        System.out.println(nombre_estudiante+apellido_estudiante);
+        System.out.println(nombre_estudiante+" "+apellido_estudiante);
+    }
+}
